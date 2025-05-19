d27da18f2281fe0ec38af7bf3b38a3dc59efb751
santosh <bmeti3718@gmail.com>
2025-05-19 23:14:26 +0530

    update script.js

--- random-joke-generator/script.js
+++ random-joke-generator/script.js
@@ -1,7 +1,11 @@
 const _url = "https://icanhazdadjoke.com/";
 const joke = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
 
-function getJoke(){
+function getJoke() {
+    jokeBtn.disabled = true;
+    joke.style.opacity = 0;
+
     fetch(_url, {
         method: 'GET',
         headers: {
@@ -10,7 +14,13 @@ function getJoke(){
     })
     .then(response => response.json())
     .then(data => {
-        joke.innerHTML = data.joke;
+        setTimeout(() => {
+            joke.innerHTML = `<h4 class="sentence">${data.joke}</h4>`;
+            joke.style.opacity = 1;
+            jokeBtn.disabled = false;
+        }, 300);
     })
 }
-getJoke()
+
+getJoke();
+jokeBtn.addEventListener("click", getJoke);
